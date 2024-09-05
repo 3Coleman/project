9c341ff3517d2a150070b9ba830cad5273462c84
abelsibi <abelx.sibi@intel.com>
2024-09-05 12:31:43 +0530

    new lines added

---
+++ DSA/practice/O of list.py
@@ -0,0 +1,30 @@
+# list1=[1,2,3,4]
+# list1.append(5)
+# print(list1)
+
+#in above cases of appending only one operation happened ,ie adding a element at the end -O(1)
+
+# list1=[1,2,3,4]
+# list1.pop()
+# print(list1)
+
+#in above cases of poping out ,only one operation happened ,ie removing an element at the end .
+
+# list1=[1,2,3,4]
+# list1.insert(0,0)
+# print(list1)
+#in above cases of inserting  an element at index 0 ,n mumber of  operation happening due to rearrangement of the indexes for rest of the existing element  ,ie O(n)
+
+# list1=[1,2,3,4]
+# list1.pop(1)
+# print(list1)
+
+#in above cases of poping out   an element at index 1 ,n mumber of  operation happening due to rearrangement of the indexes for rest of the existing element  ,ie O(n)
+
+
+O(n^2)=a ;oop within a loop
+O(n)=propotional
+O(logn)=divide and conqour
+O(1)=constant
+Big O cheat sheet 
+https://www.bigocheatsheet.com/
